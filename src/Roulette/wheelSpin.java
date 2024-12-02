0ee3ac5597bbfe70d85598401a6239221e1bbc2c
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2024-12-01 22:23:53 -0500

    First Push for Roulette
    Here I have my random number generator that creates a random integer using random import statement. Trying to test what the package will look like in github

---
+++ src/Roulette/wheelSpin.java
@@ -0,0 +1,13 @@
+package Roulette;
+
+import java.util.Random;
+
+public class wheelSpin {
+    public static int spinWheel() {
+        Random random = new Random();
+        return random.nextInt(37);
+    }
+    public static void main(String[] args) {
+        System.out.println(spinWheel());
+    }
+}
