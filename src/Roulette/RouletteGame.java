dcd6d1237759998b3d0d58d4c22166fd6bad29e8
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2024-12-02 23:40:40 -0500

    Starting to create what the user will see
    Creating a scanner to see user input and then I just set up a basic chip variable will change that when we incooperate it all together. Creating a while loop that goes until the user do not want to anymore and some logic stuff for the if statement for the chip logic

--- src/Roulette/RouletteGame.java
+++ src/Roulette/RouletteGame.java
@@ -1,5 +1,7 @@
 package Roulette;
 
+import java.util.Scanner;
+
 public class RouletteGame {
     private wheelSpin wheel;
     private ColorDeterminer colorDeterminer;
@@ -8,5 +10,27 @@ public class RouletteGame {
         this.wheel = new wheelSpin();
         this.colorDeterminer = new ColorDeterminer();
     }
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+        int chips = 100;
+        boolean playAgain = true;
 
+        while (playAgain) {
+            System.out.println("\nYou have " + chips + " chips.");
+            System.out.print("Enter the amount of chips you want to bet: ");
+            int betAmount = scanner.nextInt();
+            if (betAmount > 0 && betAmount <= chips) {
+                System.out.println("\nWelcome to the Roulette game!");
+                System.out.println("You can place the following types of bets:");
+                System.out.println("1. Straight-Up Bet (Pick a number between 0-36)");
+                System.out.println("2. Even/Odd Bet");
+                System.out.println("3. Red/Black Bet");
+                System.out.println("4. Low/High Bet (1-18 or 19-36)");
+                System.out.print("Enter the type of bet you want to place (1-4): ");
+                int betType = scanner.nextInt();
+
+            }
+        }
+    }
 }
+
