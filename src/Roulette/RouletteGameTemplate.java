7a46508f5b3ff1644fb54edce194067dcb9af3a5
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2024-12-04 21:46:59 -0500

    more comments

--- src/Roulette/RouletteGameTemplate.java
+++ src/Roulette/RouletteGameTemplate.java
@@ -2,6 +2,7 @@ package Roulette;
 //Trying to incorporate the Template Method Pattern
 public abstract class RouletteGameTemplate {
     //Might consider adding the play() method here??
+    //Going to change the name of the methods as well for better understanding
     protected abstract int getBetAmount();
 
     protected abstract int getBetType();
