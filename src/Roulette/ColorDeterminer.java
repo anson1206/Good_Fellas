de80ad0c1d2c2d000b687409abdffd39318e45f1
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2024-12-08 15:23:01 -0500

    Updated the color determiner because it was opposite
    Just changed two lines of code

--- src/Roulette/ColorDeterminer.java
+++ src/Roulette/ColorDeterminer.java
@@ -8,10 +8,10 @@ public class ColorDeterminer {
             int[] evenNumbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36};
             for (int even : evenNumbers) {
                 if (number == even) {
-                    return "Black";
+                    return "Red";
                 }
             }
-            return "Red";
+            return "Black";
         }
     }
 }
